1168198bd6f8bda344cf5752496ce079f17f12cc
Ardit <ardit.vula@gmail.com>
2017-01-11 23:30:36 +0100

    Second commit

--- db/migrate/20170111220840_create_restaurants.rb
+++ db/migrate/20170111220840_create_restaurants.rb
@@ -3,9 +3,7 @@ class CreateRestaurants < ActiveRecord::Migration[5.0]
     create_table :restaurants do |t|
       t.string :name
       t.text :description
-      t.decimal :rating
       t.text :address
-
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170111221141) do
+ActiveRecord::Schema.define(version: 20170111222929) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -38,10 +38,16 @@ ActiveRecord::Schema.define(version: 20170111221141) do
     t.datetime "updated_at",   null: false
   end
 
+  create_table "restaurant_ratings", force: :cascade do |t|
+    t.integer  "restaurant_id"
+    t.decimal  "rating"
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
+  end
+
   create_table "restaurants", force: :cascade do |t|
     t.string   "name"
     t.text     "description"
-    t.decimal  "rating"
     t.text     "address"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
